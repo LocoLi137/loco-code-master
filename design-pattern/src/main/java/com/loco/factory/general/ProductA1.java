4e109febb127decc2a52a81894a3ddc437daa287
locoli <563235247@qq.com>
2023-07-25 14:33:10 +0800

    初始化项目、抽象工厂模板代码

---
+++ design-pattern/src/main/java/com/loco/factory/general/ProductA1.java
@@ -0,0 +1,13 @@
+package com.loco.factory.general;
+
+/**
+ * 产品A的实现类
+ */
+public class ProductA1 extends AbstractProductA {
+
+	@Override
+	public void doSomething() {
+		System.out.println("产品A1的实现方法");
+	}
+
+}
