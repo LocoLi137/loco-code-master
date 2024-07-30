d1e87eeecd2da99f832d65213e4dbcdab010e650
loco <563235247@qq.com>
2024-07-30 20:23:51 +0800

    Buffer scatteringAndGathering

--- netty-demo/src/main/java/com/loco/nio/BufferDemo.java
+++ netty-demo/src/main/java/com/loco/nio/BufferDemo.java
@@ -1,9 +1,13 @@
 package com.loco.nio;
 
 import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
 
 /**
  * description: buffer 特性
@@ -15,7 +19,46 @@ public class BufferDemo {
     public static void main(String[] args) throws Exception {
 //        byteBufferPutAndGet();
 //        readOnlyBuffer();
-        superBuffer();
+//        superBuffer();
+        scatteringAndGathering();
+    }
+
+    private static void scatteringAndGathering() throws Exception{
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(7000);
+        serverSocketChannel.socket().bind(inetSocketAddress);
+        //创建 buffer 数组
+        ByteBuffer[] byteBuffers = new ByteBuffer[2];
+        byteBuffers[0] = ByteBuffer.allocate(5);
+        byteBuffers[1] = ByteBuffer.allocate(3);
+        //等客户端连接(telnet)
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        int messageLength = 8; //假定从客户端接收 8 个字节
+        //循环的读取
+        while (true) {
+            int byteRead = 0;
+            while (byteRead < messageLength ) {
+                long l = socketChannel.read(byteBuffers);
+                byteRead += (int) l; //累计读取的字节数
+                System.out.println("byteRead=" + byteRead);
+                //使用流打印, 看看当前的这个 buffer 的 position 和 limit
+                Arrays.stream(byteBuffers).map(buffer -> "position=" + buffer.position() + ", limit=" +
+                        buffer.limit()).forEach(System.out::println);
+            }
+            //将所有的 buffer 进行 flip
+            Arrays.asList(byteBuffers).forEach(ByteBuffer::flip);
+            //将数据读出显示到客户端
+            long byteWirte = 0;
+            while (byteWirte < messageLength) {
+                long l = socketChannel.write(byteBuffers); //
+                byteWirte += l;
+            }
+            //将所有的 buffer 进行 clear
+            Arrays.asList(byteBuffers).forEach(ByteBuffer::clear);
+            System.out.println("byteRead:=" + byteRead + " byteWrite=" + byteWirte + ", messagelength" +
+                    messageLength);
+        }
+
     }
 
     /**
@@ -25,12 +68,12 @@ public class BufferDemo {
         RandomAccessFile randomAccessFile = new RandomAccessFile(classpath+"super01.txt", "rw");
         //获取对应的通道
         FileChannel channel = randomAccessFile.getChannel();
-        /**
-         * 参数 1: FileChannel.MapMode.READ_WRITE 使用的读写模式
-         * 参数 2： 0 ： 可以直接修改的起始位置
-         * 参数 3: 5: 是映射到内存的大小(不是索引位置) ,即将 1.txt 的多少个字节映射到内存
-         * 可以直接修改的范围就是 0-5
-         * 实际类型 DirectByteBuffer
+        /*
+          参数 1: FileChannel.MapMode.READ_WRITE 使用的读写模式
+          参数 2： 0 ： 可以直接修改的起始位置
+          参数 3: 5: 是映射到内存的大小(不是索引位置) ,即将 1.txt 的多少个字节映射到内存
+          可以直接修改的范围就是 0-5
+          实际类型 DirectByteBuffer
          */
         MappedByteBuffer mappedByteBuffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, 5);
         mappedByteBuffer.put(0, (byte) 'H');
