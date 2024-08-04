7ef33712c8d94e0d8830802575511a0cad9754a1
loco <563235247@qq.com>
2024-08-04 21:18:14 +0800

    nio selector server 临时提交

--- netty-demo/src/main/java/com/loco/nio/NIOServer.java
+++ netty-demo/src/main/java/com/loco/nio/NIOServer.java
@@ -1,26 +1,74 @@
 package com.loco.nio;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.nio.channels.spi.SelectorProvider;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * description:
  * author: Loco.Li
  */
 public class NIOServer {
-    public static void main(String[] args) {
-        allocateTest();
+    public static void main(String[] args) throws Exception {
+        run();
     }
-    //一个整型的Buffer静态变量
-    static IntBuffer intBuffer = null;
-    public static void allocateTest() {
-        //创建了一个Intbuffer实例对象
-        intBuffer = IntBuffer.allocate(20);
-        System.out.println("------------after allocate------------------");
-        System.out.println("position=" + intBuffer.position());
-        System.out.println("limit=" + intBuffer.limit());
-        System.out.println("capacity=" + intBuffer.capacity());
+
+    private static void run() throws Exception{
+        //创建 ServerSocketChannel -> ServerSocket
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        //得到一个 Selector 对象
+        Selector selector = Selector.open();
+        //绑定一个端口 6666, 在服务器端监听
+        serverSocketChannel.socket().bind(new InetSocketAddress(6666));
+        //设置为非阻塞
+        serverSocketChannel.configureBlocking(false);
+        //把 serverSocketChannel 注册到 selector 关心 事件为 OP_ACCEPT
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        //循环等待客户端连接
+        while (true) {
+            //等待 1s，如果没有事件发生, 返回
+            if (selector.select(1000) == 0) { //无事发生
+                System.out.println("服务器等待了 1 秒，无连接");
+                continue;
+            }
+            // 如果返回的>0, 表示已经获取到关注的事件：相关的 selectionKey 集合  (selector.selectedKeys() 返回关注事件的集合)
+            // 通过 selectionKeys 反向获取通道
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            //遍历 Set<SelectionKey>, 使用迭代器遍历
+            Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
+            while (keyIterator.hasNext()) {
+                //获取到 SelectionKey
+                SelectionKey key = keyIterator.next();
+                //根据 key 对应的通道发生的事件做相应处理
+                if (key.isAcceptable()) { //如果是 OP_ACCEPT, 有新的客户端连接
+                    //该该客户端生成一个 SocketChannel
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    System.out.println(" 客 户 端 连 接 成 功 生 成 了 一 个 socketChannel " + socketChannel.hashCode());
+                    //将 SocketChannel 设置为非阻塞
+                    socketChannel.configureBlocking(false);
+                    //将 socketChannel 注册到 selector, 关注事件为 OP_READ， 同时给 socketChannel
+                    //关联一个 Buffer
+                    socketChannel.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                }
+                if (key.isReadable()) { //发生 OP_READ
+                    //通过 key 反向获取到对应 channel
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    //获取到该 channel 关联的 buffer
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    channel.read(buffer);
+                    System.out.println("form 客户端 " + new String(buffer.array()));
+                }
+                //手动从集合中移动当前的 selectionKey, 防止重复操作
+                keyIterator.remove();
+            }
+        }
     }
 }
